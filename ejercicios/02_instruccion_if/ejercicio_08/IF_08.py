b8cba293f355ee5da257c46d8116d34675656d6a
Lucas <lucasabeijon92@gmail.com>
2023-07-11 09:46:43 -0300

    Ejercicio 8 if

--- ejercicios/02_instruccion_if/ejercicio_08/IF_08.py
+++ ejercicios/02_instruccion_if/ejercicio_08/IF_08.py
@@ -41,7 +41,13 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        edad = int(self.txt_edad.get())
+        estado_civil = self.combobox_estado_civil.get()
+        
+        if edad >= 18 and estado_civil == "Soltero":
+            mensaje = "Es soltero y no es menor"
+
+        alert(title="Ejercicio 8", message=mensaje)
 
         
         
