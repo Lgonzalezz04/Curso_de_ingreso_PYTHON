0c5f3f804b7cc9c3c00fea40eeb72b034094a837
Lucas <lucasabeijon92@gmail.com>
2023-07-12 11:03:46 -0300

    TP 4

--- ejercicios/07_tps/04_IF_ferrete_iluminacion/app.py
+++ ejercicios/07_tps/04_IF_ferrete_iluminacion/app.py
@@ -32,52 +32,50 @@ class App(customtkinter.CTk):
 
         self.combobox_cantidad = customtkinter.CTkComboBox(master=self, values= ["1", "2","3","4","5","6","7","8","9","10","11","12"])
         self.combobox_cantidad.grid(row=1, column=1, padx=10, pady=10)
-                
+        
         self.btn_calcular = customtkinter.CTkButton(master=self, text="Calcular", command=self.btn_calcular_on_click)
         self.btn_calcular.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
 
     def btn_calcular_on_click(self):
         marca = self.combobox_marca.get()
-        cantidad_lamparitas = int(self.combobox_cantidad.get())
-        precio = int(800) * cantidad_lamparitas
+        cantidad = int(self.combobox_cantidad.get())
+        precio = 800 
 
         #A. Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%.
-        if cantidad_lamparitas >= 6:
-            descuento = precio - (precio * 50 / 100)
-            mensaje = str(descuento)
+        if cantidad >=6:
+            precio = (precio * .50) * cantidad
 
         #B. Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
-        if cantidad_lamparitas == 5 and marca == "ArgentinaLuz":
-            descuento = precio - (precio * 40 / 100)
-            mensaje = str(descuento)
-        elif cantidad_lamparitas == 5 and marca != "ArgentinaLuz":
-            descuento = precio - (precio * 30 /100)
-            mensaje = str(descuento)
+        elif cantidad == 5:
+            precio = precio * cantidad
+            if marca == "ArgentinaLuz":
+                precio = (precio * .60)
+            else:
+                precio = (precio * .70)
 
         #C. Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
-        if cantidad_lamparitas == 4 and marca == "ArgentinaLuz" or cantidad_lamparitas == 4 and marca == "FelipeLamparas":
-            descuento = precio - (precio * 25 /100)
-            mensaje = str(descuento)
-        elif cantidad_lamparitas == 4 and marca != "ArgentinaLuz" or cantidad_lamparitas == 4 and marca != "FelipeLamparas":
-            descuento = precio - (precio * 20 /100)
-            mensaje = str(descuento)
+        elif cantidad == 4:
+            precio = precio * cantidad
+            if marca in["ArgentinaLuz","FelipeLamparas"]:
+                precio = (precio * .75)
+            else:
+                precio = (precio * .80)
 
         #D. Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-        if cantidad_lamparitas == 3 and marca == "ArgentinaLuz":
-            descuento = precio - (precio *15 /100)
-            mensaje = str(descuento)
-        elif cantidad_lamparitas == 3 and marca == "FelipeLamparas":
-            descuento = precio - (precio *10 /100)
-            mensaje = str(descuento)
-        elif cantidad_lamparitas == 3 and marca != "ArgentinaLuz" or cantidad_lamparitas == 3 and marca != "FelipeLamparas":
-            descuento = precio - (precio *5 /100)
-            mensaje = str(descuento)
-        
+        elif cantidad == 3:
+            precio = precio * cantidad
+            if marca == "ArgentinaLuz":
+                precio = (precio * .85)
+            elif marca == "FelipeLamparas":
+                precio = (precio * .90)
+            else:
+                percio = (precio * .95)
+
         #E. Si el importe final con descuento suma más de $4000  se obtien un descuento adicional de 5%.
-        if precio >4000:
-            descuento = precio - (precio *5 /100)
-            mensaje = str(descuento)
+        if precio  > 4000:
+            precio = (precio * .95)
+        mensaje = f"El total seria: {precio}"
         alert(title="TP 4",message=mensaje)
         
     
