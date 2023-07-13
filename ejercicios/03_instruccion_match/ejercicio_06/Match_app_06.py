36ba863b59643dc4f89f206d71572e6d298f04f9
Lucas <lucasabeijon92@gmail.com>
2023-07-13 11:16:04 -0300

    Match 06

--- ejercicios/03_instruccion_match/ejercicio_06/Match_app_06.py
+++ ejercicios/03_instruccion_match/ejercicio_06/Match_app_06.py
@@ -6,6 +6,8 @@ import customtkinter
 
 
 '''
+Apellido: Gonzalez Abeijon
+Nombre: Lucas
 Enunciado:
 Obtener la hora ingresada en el cuadro de texto txt_hora. 
 Al presionar el botón ‘Informar’ mostrar mediante alert alguno de los 
@@ -34,7 +36,19 @@ class App(customtkinter.CTk):
         
     
     def btn_informar_on_click(self):
-        pass
+        hora = self.txt_hora.get()
+
+        match hora:
+            case "7" | "8" | "9" | "10" | "11":
+                mensaje = "Es de mañana"
+            case "12" | "13" | "14" | "15" | "16" | "17" | "18" | "19":
+                mensaje = "Es de tarde"
+            case "0" | "1" | "2" | "3" | "4" | "5" | "6" | "20" | "21" | "22" | "23" | "24":
+                mensaje = "Es de noche"
+            case _:
+                mensaje = "Esa hora no existe"
+        
+        alert(title="Match 06", message=mensaje)
     
     
 if __name__ == "__main__":
