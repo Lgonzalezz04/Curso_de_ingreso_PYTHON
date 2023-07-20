56f292120b0b951a10e72d8440ab09cf52134045
Lucas <lucasabeijon92@gmail.com>
2023-07-20 09:06:40 -0300

    Ej 10 while

--- ejercicios/04_intruccion_while/ejercicio_10/While_app_10.py
+++ ejercicios/04_intruccion_while/ejercicio_10/While_app_10.py
@@ -5,6 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
+Apellido: Gonzalez Abeijon
+Nombre:Lucas
 Enunciado:
 Al presionar el botón ‘Comenzar ingreso’, solicitar mediante prompt todos los números que el usuario 
 quiera hasta que presione el botón Cancelar (en el prompt). 
@@ -33,8 +35,26 @@ class App(customtkinter.CTk):
 
 
     def btn_comenzar_ingreso_on_click(self):
-        pass
-
+        suma_negativos = 0
+        cantidad_negativos = 0
+        suma_positivos = 0
+        cantidad_positivos = 0
+        cantidad_ceros = 0
+        while True:
+            numero = prompt(title="Ej 10", prompt="Ingrese un numero")
+            if numero == None:
+                break
+            numero = int(numero)
+            if numero > 0:
+                cantidad_positivos += 1
+                suma_positivos += numero
+            elif numero == 0:
+                cantidad_ceros += 1
+            else:
+                cantidad_negativos += 1
+                suma_negativos -=numero
+            diferencia = cantidad_positivos - cantidad_negativos
+            alert(title="Ej 10",message="Suma positivos: {0}\n Suma negativos: -{1}\n Cantidad Positivos: {2}\n Cantidad Negativos: {3}\n Cantidad de Ceros: {4}\n Diferencia: {5}".format(suma_positivos,suma_negativos,cantidad_positivos,cantidad_negativos,cantidad_ceros,diferencia))
     
 if __name__ == "__main__":
     app = App()
